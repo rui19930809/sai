f3309f104c858a297503da6aff4517dd214844d1
rui_li <rui_li@accton.com.cn>
2021-04-27 03:28:57 +0000

    add file

---
+++ build/linux-board/ctcsai/CMakeFiles/sai.dir/DependInfo.cmake
@@ -0,0 +1,104 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/sai/ctcplatform/driver/fiber_handle/fiber_handle.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/__/ctcplatform/driver/fiber_handle/fiber_handle.c.o"
+  "/sai/ctcplatform/driver/i2c_handle/i2c_cpm.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/__/ctcplatform/driver/i2c_handle/i2c_cpm.c.o"
+  "/sai/ctcplatform/driver/i2c_handle/i2c_handle.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/__/ctcplatform/driver/i2c_handle/i2c_handle.c.o"
+  "/sai/ctcplatform/driver/macled_handle/macled_handle.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/__/ctcplatform/driver/macled_handle/macled_handle.c.o"
+  "/sai/ctcplatform/mgt/cJSON.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/__/ctcplatform/mgt/cJSON.c.o"
+  "/sai/ctcplatform/mgt/ctc_board_e530_24x2c.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/__/ctcplatform/mgt/ctc_board_e530_24x2c.c.o"
+  "/sai/ctcplatform/mgt/ctc_board_e530_48s4x.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/__/ctcplatform/mgt/ctc_board_e530_48s4x.c.o"
+  "/sai/ctcplatform/mgt/ctc_board_e530_48t4x_p.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/__/ctcplatform/mgt/ctc_board_e530_48t4x_p.c.o"
+  "/sai/ctcplatform/mgt/ctc_board_e550_24t16y.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/__/ctcplatform/mgt/ctc_board_e550_24t16y.c.o"
+  "/sai/ctcplatform/mgt/ctc_board_json.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/__/ctcplatform/mgt/ctc_board_json.c.o"
+  "/sai/ctcplatform/mgt/ctc_sai_platform.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/__/ctcplatform/mgt/ctc_sai_platform.c.o"
+  "/sai/ctcsai/src/ctc_redis_client.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_redis_client.c.o"
+  "/sai/ctcsai/src/ctc_sai.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai.c.o"
+  "/sai/ctcsai/src/ctc_sai_acl.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_acl.c.o"
+  "/sai/ctcsai/src/ctc_sai_app_cfg.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_app_cfg.c.o"
+  "/sai/ctcsai/src/ctc_sai_app_cfg_chip_profile.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_app_cfg_chip_profile.c.o"
+  "/sai/ctcsai/src/ctc_sai_app_cfg_datapath_profile.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_app_cfg_datapath_profile.c.o"
+  "/sai/ctcsai/src/ctc_sai_app_cfg_parse.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_app_cfg_parse.c.o"
+  "/sai/ctcsai/src/ctc_sai_bridge.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_bridge.c.o"
+  "/sai/ctcsai/src/ctc_sai_buffer.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_buffer.c.o"
+  "/sai/ctcsai/src/ctc_sai_db.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_db.c.o"
+  "/sai/ctcsai/src/ctc_sai_dtel.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_dtel.c.o"
+  "/sai/ctcsai/src/ctc_sai_fdb.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_fdb.c.o"
+  "/sai/ctcsai/src/ctc_sai_hostif.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_hostif.c.o"
+  "/sai/ctcsai/src/ctc_sai_isolation_group.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_isolation_group.c.o"
+  "/sai/ctcsai/src/ctc_sai_lag.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_lag.c.o"
+  "/sai/ctcsai/src/ctc_sai_ld_hash.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_ld_hash.c.o"
+  "/sai/ctcsai/src/ctc_sai_mcast.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_mcast.c.o"
+  "/sai/ctcsai/src/ctc_sai_mirror.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_mirror.c.o"
+  "/sai/ctcsai/src/ctc_sai_mpls.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_mpls.c.o"
+  "/sai/ctcsai/src/ctc_sai_neighbor.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_neighbor.c.o"
+  "/sai/ctcsai/src/ctc_sai_next_hop.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_next_hop.c.o"
+  "/sai/ctcsai/src/ctc_sai_next_hop_group.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_next_hop_group.c.o"
+  "/sai/ctcsai/src/ctc_sai_oid.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_oid.c.o"
+  "/sai/ctcsai/src/ctc_sai_policer.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_policer.c.o"
+  "/sai/ctcsai/src/ctc_sai_port.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_port.c.o"
+  "/sai/ctcsai/src/ctc_sai_qosmap.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_qosmap.c.o"
+  "/sai/ctcsai/src/ctc_sai_queue.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_queue.c.o"
+  "/sai/ctcsai/src/ctc_sai_route.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_route.c.o"
+  "/sai/ctcsai/src/ctc_sai_router_interface.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_router_interface.c.o"
+  "/sai/ctcsai/src/ctc_sai_samplepacket.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_samplepacket.c.o"
+  "/sai/ctcsai/src/ctc_sai_scheduler.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_scheduler.c.o"
+  "/sai/ctcsai/src/ctc_sai_scheduler_group.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_scheduler_group.c.o"
+  "/sai/ctcsai/src/ctc_sai_stp.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_stp.c.o"
+  "/sai/ctcsai/src/ctc_sai_switch.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_switch.c.o"
+  "/sai/ctcsai/src/ctc_sai_tam.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_tam.c.o"
+  "/sai/ctcsai/src/ctc_sai_tunnel.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_tunnel.c.o"
+  "/sai/ctcsai/src/ctc_sai_udf.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_udf.c.o"
+  "/sai/ctcsai/src/ctc_sai_virtual_router.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_virtual_router.c.o"
+  "/sai/ctcsai/src/ctc_sai_vlan.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_vlan.c.o"
+  "/sai/ctcsai/src/ctc_sai_warmboot.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_warmboot.c.o"
+  "/sai/ctcsai/src/ctc_sai_warmboot_func.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_warmboot_func.c.o"
+  "/sai/ctcsai/src/ctc_sai_wred.c" "/sai/build/linux-board/ctcsai/CMakeFiles/sai.dir/src/ctc_sai_wred.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CONFIG_DBCLIENT"
+  "SDK_IN_USERMODE"
+  "SDK_WORK_ENV=1"
+  "SDK_WORK_PLATFORM=0"
+  "TSINGMA"
+  "_CTC_ARM_CTC5236_"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../../SAI/inc"
+  "../../ctcsai/inc"
+  "../../ctcsdk/core"
+  "../../ctcsdk/sal/include"
+  "../../ctcsdk/dal/include"
+  "../../ctcsdk/core/init"
+  "../../ctcsdk/core/app"
+  "../../ctcsdk/core/common/include"
+  "../../ctcsdk/dkits"
+  "../../ctcsdk/core/api/include"
+  "../../ctcsai/../ctcplatform/inc"
+  "../../ctcsai/../ctcplatform/driver/i2c_handle"
+  "../../ctcsai/../ctcplatform/driver/fiber_handle"
+  "../../ctcsai/../ctcplatform/driver/macled_handle"
+  "../../ctcsai/../ctcplatform/mgt"
+  )
+
+# Pairs of files generated by the same build rule.
+set(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/sai/build/lib/linux-board/tsingma/libsai.so" "/sai/build/lib/linux-board/tsingma/libsai.so.1"
+  "/sai/build/lib/linux-board/tsingma/libsai.so.1.5.2" "/sai/build/lib/linux-board/tsingma/libsai.so.1"
+  )
+
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
